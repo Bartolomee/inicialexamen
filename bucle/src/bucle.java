fcd04cda69bdf595528bebf90b045b2032b2cc2a
Mal Sape <jose5598vlc@gmail.com>
2017-11-24 14:34:05 +0100

    añadimos resto

---
+++ bucle/src/bucle.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class bucle {
+
+	public static void main(String[] args) {
+		// bucle 
+		
+		int i = 0;
+		int num = 2; 
+		int tope;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("introduce el numero tope");
+		tope=sc.nextInt();
+		if (tope>=0)
+		{		
+		while (i<=tope)
+		{
+		System.out.println(num);
+		num=num+2;
+		i++;
+		
+		}
+		
+	}
+		
+	}
+	
+}
